d2cd65455f14de58dd0ec1583296e213bc5a8b65
Richard Wilson <r.crawfordwilson@gmail.com>
2013-05-12 22:03:21 -0700

    Cleanup

--- main.rb
+++ main.rb
@@ -1,7 +1,1 @@
-def n_times(thing)
-  lambda {|n| puts "first called"; thing * n}  
-  lambda {|n| puts "second called"; thing - n}
-end
-
-test = n_times(2)
-puts test.call(3)
+#Working file for misc practice
