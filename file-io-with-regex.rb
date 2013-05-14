fb74dcf7557788bd202bde4334dfdb6e242da4a3
Richard Wilson <r.crawfordwilson@gmail.com>
2013-05-14 16:41:13 -0700

    File IO with regex

---
+++ file-io-with-regex.rb
@@ -0,0 +1,24 @@
+#Prepared by Richard Wilson
+#While studying Programming Ruby 1.9
+#TODO:
+#Open file-io-samples/MultipleLinesCustomDelimiter.txt
+#Break each line into fields
+#Convert Times from mm:ss to seconds
+#Remove any redundant spaces.
+#######################################################
+
+Song = Struct.new(:title, :name, :length)
+songs = []
+
+File.open("file-io-samples/MultipleLinesCustomDelimiter.txt") do |file_data|
+  file_data.each do |line|
+    file, length, name, title =  line.chomp.split(/\s*\|\s*/)
+    name.squeeze!(" ") 
+    mins, secs = length.scan(/\d+/).collect {|x| x.to_i}
+    secs += mins * 60
+    songs << Song.new(title, name, secs.to_s)
+  end
+end
+
+p songs
+  
