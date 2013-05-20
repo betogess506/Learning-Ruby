a84d172207d67bd393fbc82c56e6c579e38db46a
Richard Wilson <r.crawfordwilson@gmail.com>
2013-05-20 15:31:40 -0700

    Experimenting with Threading

---
+++ Threading.rb
@@ -0,0 +1,13 @@
+#Venturing into the depths of multithreading.
+require 'net/http'
+pages = %w{ www.rubycentral.com google.ca slashdot.org }
+threads = Array.new
+
+pages.each do |page|
+  threads << Thread.new(page) do |url|
+    href = Net::HTTP.new(url, 80)
+    response = href.get('/')
+    puts "Got #{url}: #{response.message}"
+  end
+end
+threads.each {|thread| thread.join}
