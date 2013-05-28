c8472b81e77badf7bf11de1e3bca70c20c1f0ca3
Senjai <r.crawfordwilson@gmail.com>
2013-05-27 23:34:31 -0700

    Adding singleton example, they still confuse me. Asked for advice on stackoverflow (see comment in file)

---
+++ snippets/singletons.rb
@@ -0,0 +1,24 @@
+#Spawned question on stack overflow here: http://stackoverflow.com/questions/16785239/why-do-we-prefix-class-variables-with-in-ruby
+
+class Test
+  @var = 99
+  attr_accessor :vars
+
+  def initialize
+    @var = "Whoop"
+  end
+
+  def pri
+    puts @var
+  end
+
+  class << self
+    attr_accessor :var
+  end
+end
+
+t = Test.new
+puts Test.var
+Test.var = 5
+puts Test.var
+puts t.pri
