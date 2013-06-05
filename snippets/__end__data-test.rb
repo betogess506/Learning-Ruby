4d54a3a0a82564f9a65db763ae37d1812fd16477
Senjai <r.crawfordwilson@gmail.com>
2013-06-04 20:51:15 -0700

    Added an awesome little snippet that I think is FANTASTIC for writing other snippets, I know longer need to add files with simple file reading snippets.

---
+++ snippets/__end__data-test.rb
@@ -0,0 +1,9 @@
+#This is really cool for creating snippits that only need to read from one file.
+#Excellent, run it to see what I'm talking about!
+
+puts DATA.read
+puts DATA.class
+__END__
+this is a test
+string of data,
+that should be stored in data?
