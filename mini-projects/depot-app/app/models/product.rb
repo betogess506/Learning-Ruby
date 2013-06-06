a576ccf9b080d270e12061aefd6ee1cde719d9f6
Senjai <r.crawfordwilson@gmail.com>
2013-06-06 14:23:39 -0700

    Got line items and carts working together, ish. Need to add functionality. I hate mass alignment errors Bleugh..

--- mini-projects/depot-app/app/models/product.rb
+++ mini-projects/depot-app/app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
+  has_many :line_items
+  attr_accessible :description, :image_url, :price, :title, :line_items
+
 
   validates :description, :title, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -8,4 +10,17 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i,
     message: 'Must be a valid URL for a gif, png, or jpg..'
   }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items Present')
+      return false
+    end
+  end
 end
