180a95034b6a63bbc49d5b6925c67b36e94bcdd0
Senjai <r.crawfordwilson@gmail.com>
2013-06-06 15:33:13 -0700

    Cart works, added a little tracking widget using sessions (displays if index is accessed 5 or more times before adding a item to the cart (resets on adding an item)

--- mini-projects/depot-app/app/controllers/store_controller.rb
+++ mini-projects/depot-app/app/controllers/store_controller.rb
@@ -2,5 +2,14 @@ class StoreController < ApplicationController
   def index
     # Order products by their title
     @products = Product.order(:title)
+
+    unless session[:counter].nil?
+      session[:counter] += 1
+    else
+      session[:counter] = 1
+    end
+
+    @count = session[:counter]
+
   end
 end
