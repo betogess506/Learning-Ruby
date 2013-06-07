667cbd9111418c9333811c64ef3a570ca99933dd
Senjai <r.crawfordwilson@gmail.com>
2013-06-06 21:17:54 -0700

    Added price column to LineItem table, changed model/controller to reflect it.

---
+++ mini-projects/depot-app/db/migrate/20130607040205_add_product_price_to_line_item.rb
@@ -0,0 +1,9 @@
+class AddProductPriceToLineItem < ActiveRecord::Migration
+  def change
+    add_column :line_items, :price, :decimal, precision: 8, scale: 2
+
+    LineItem.all.each do |lineitem|
+      lineitem.update_attribute :price, lineitem.product.price
+    end
+  end
+end
